5199250070c35d891db6482e6fae78ecddb35d6c
Accusoft <support@accusoft.com>
2024-03-30 11:06:50 +0000

    13.27 Release

--- src/js/viewer.js
+++ src/js/viewer.js
@@ -1,4 +1,4 @@
-// Copyright (C) 1996-2023 Accusoft Corporation
+// Copyright (C) 1996-2024 Accusoft Corporation
 // See https://github.com/Accusoft/prizmdoc-viewer/blob/master/LICENSE
 
 //---------------------------------------------------------------------------------------------------------------------
